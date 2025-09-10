a90b4d6c3ee68b46f855b59a5f2d32aa09958afb
Jonathan Thomas <jonathan@openshot.org>
2025-09-10 17:11:41 -0500

    Improving fish eye support for SphericalProjection effect (4 types of fish eyes for input and output). Fixing lots of issues with virtual camera controls (so controls don't flip axis randomly)

--- src/effects/SphericalProjection.h
+++ src/effects/SphericalProjection.h
@@ -34,10 +34,40 @@ private:
   void init_effect_details();
 
 public:
+  // Enums
+  enum InputModel {
+    INPUT_EQUIRECT          = 0,
+    INPUT_FEQ_EQUIDISTANT   = 1, // r = f * theta
+    INPUT_FEQ_EQUISOLID     = 2, // r = 2f * sin(theta/2)
+    INPUT_FEQ_STEREOGRAPHIC = 3, // r = 2f * tan(theta/2)
+    INPUT_FEQ_ORTHOGRAPHIC  = 4  // r = f * sin(theta)
+  };
+
+  enum ProjectionMode {
+    MODE_RECT_SPHERE            = 0, // Rectilinear view over full sphere
+    MODE_RECT_HEMISPHERE        = 1, // Rectilinear view over hemisphere
+    MODE_FISHEYE_EQUIDISTANT    = 2, // Output fisheye (equidistant)
+    MODE_FISHEYE_EQUISOLID      = 3, // Output fisheye (equisolid)
+    MODE_FISHEYE_STEREOGRAPHIC  = 4, // Output fisheye (stereographic)
+    MODE_FISHEYE_ORTHOGRAPHIC   = 5  // Output fisheye (orthographic)
+  };
+
+  enum InterpMode {
+    INTERP_NEAREST  = 0,
+    INTERP_BILINEAR = 1,
+    INTERP_BICUBIC  = 2,
+    INTERP_AUTO     = 3
+  };
+
+  enum InvertFlag {
+    INVERT_NORMAL = 0,
+    INVERT_BACK   = 1
+  };
+
   Keyframe yaw;    ///< Yaw around up-axis (degrees)
   Keyframe pitch;  ///< Pitch around right-axis (degrees)
   Keyframe roll;   ///< Roll around forward-axis (degrees)
-  Keyframe fov;    ///< Output field-of-view (horizontal, degrees)
+  Keyframe fov;    ///<  Output field-of-view (degrees)
   Keyframe in_fov; ///< Source lens coverage / FOV (degrees)
 
   int projection_mode; ///< 0=Sphere, 1=Hemisphere, 2=Fisheye
